97a12309987ecbb8f5a9505ebd574f955737fb73
matheusoliveira1 <matheus.oliveira@datenworks.com>
2022-03-25 21:41:25 -0300

    Remove unusable code

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -20,15 +20,6 @@ public class TicketMachine {
     }
 
     public void inserir(int quantia) throws PapelMoedaInvalidaException {
-        boolean achou = false;
-        for (int i = 0; i < papelMoeda.length && !achou; i++) {
-            if (papelMoeda[1] == quantia) {
-                achou = true;
-            }
-        }
-        if (!achou) {
-            throw new PapelMoedaInvalidaException();
-        }
         this.saldo += quantia;
     }
 
